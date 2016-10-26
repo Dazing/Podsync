d72ce6701842b21f3fff755d20e5fd180e504968
Carl-Henrik Braw <callebraw@gmail.com>
2016-10-26 20:28:24 +0200

    Updates on fail managment

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ var feed = require("feed-read");
 var spawnSync = require('child_process').spawnSync;
 var fs = require('fs');
 var wget;
-var filename = "";
-var filetitle = "";
-var fileexist = false;
+var fileName = '';
+var fileTitle = '';
+var fileExist = false;
+var faillog = 'failog.txt';
+var failedList = [];
+var failogLines;
 
-podfolder = '/srv/samba/ArcServer/Media/Podcasts/';
+var podFolder = '/srv/samba/ArcServer/Media/Podcasts/';
 
 rss_url = 'http://www.bbc.co.uk/programmes/p002vsnb/episodes/downloads.rss';
 
@@ -22,56 +25,66 @@ feed(rss_url, function(err, articles) {
   		console.log(articles[0].title.replace(/[^a-zA-Z ]/g, '').replace(/ /gi,'_'));
 
   		var opt = {
-  			cwd: podfolder
+  			cwd: podFolder
   		}
   		console.log("df");
 
   		for (var i = 0; i < articles.length-1; i++) {
-  			// Remove all special characters but space
-   			filetitle = articles[i].title.replace(/[^a-zA-Z ]/g, '');
-   			// Replace all spaces with underscore
-  			filetitle = filetitle.replace(/ /gi,'_')
-  			// Append '.mp3'
-  			filename = filetitle + ".mp3";
-			fileexist = fs.existsSync(podfolder+filename)
+   			fileTitle = fileNameFormater(articles[i].title);
 
-  			console.log("i:"+i+", "+filename+", "+fileexist+"\n");
+			fileExist = fs.existsSync(podFolder+fileName)
 
-  			// If file does exist, skip to the next episode!
-  			if (fileexist) {
-  				continue;
+  			console.log("i:"+i+", "+fileName+", "+fileExist+"\n");
+
+  			// If file does exist, end loop and retry previusly failed pods
+  			if (fileExist) {
+				failedList.push(JSON.stringify(articles[i]))
+  				break;
   			}
   			else {
 				wget = spawnSync(
 						'wget',
 						[	'--no-verbose',
-							'--output-document='+filename,
+							'--output-document='+fileName,
 							articles[i].link
 							],
 						opt
 					);
 
 				// If wget failed: try once more.
-                // TODO Add failed to list and stop downloading on fileexists
+                // TODO Add failed to list and stop downloading on fileExists
                 // TODO And then check failed "download.log".
 				if (wget.status != 0) {
-					wget = spawnSync(
-							'wget',
-							[	'--no-verbose',
-								'--output-document='+filename,
-								articles[i].link
-								],
-							opt
-						);
+					failedList.push(JSON.stringify(articles[i]));
+				} // End wget failed statement
+
+  			} // End file does not exist statement
+
+  		} // End RSS entry loop
+
+		failogLines = fs.readFileSync(failog).toString().split('\n');
+		var tmpLineLink;
+		for (var i = 0; i < failogLines.length; i++) {
+			for (var i = 0; i < failedList.length; i++) {
+				tmpLineLink = faillogLines.split(' ');
+				if (failedList[i] === tmpLineLink) {
+
+					console.log(failedList[i]);
 				}
-
-  			}
-
-  		}
-
+			}
+		}
   	}
 });
 
+function fileNameFormater(title) {
+	// Remove all special characters but space
+	fileTitle = articles[i].title.replace(/[^a-zA-Z ]/g, '');
+	// Replace all spaces with underscore
+	fileTitle = fileTitle.replace(/ /gi,'_')
+	// Append '.mp3'
+	fileName = fileTitle + '.mp3';
+	return fileName;
+}
 
 /*
 
@@ -79,4 +92,11 @@ fs.readFileSync("file.txt").toString().split("\n").forEach(function(line, index,
   if (index === arr.length - 1 && line === "") { return; }
   console.log(index + " " + line);
 });
+
+failog = fs.readFileSync("file.txt").toString().split("\n");
+
+for (var i = 0; i < faillog.length; i++) {
+	faillog[i]
+}
+
 */
